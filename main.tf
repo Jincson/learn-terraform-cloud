d26a67d9853c655dc5ab72abcbc860607f20979a
Jincson <100882060+Jincson@users.noreply.github.com>
2023-01-11 01:15:30 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -36,6 +36,15 @@ resource "aws_instance" "ubuntu" {
 }
 */
 
+terraform {
+  required_providers {
+    docker = {
+      source = "kreuzwerker/docker"
+      version = "~> 2.13.0"
+    }
+  }
+}
+
 provider "docker" {}
 
 resource "docker_image" "nginx" {
@@ -51,4 +60,4 @@ resource "docker_container" "nginx" {
     external = 8000
   }
 }
-}
+
