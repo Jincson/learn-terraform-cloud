93404f876405d21e0e63153f0551e0a9f814b85f
Jincson <100882060+Jincson@users.noreply.github.com>
2023-01-11 01:21:21 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,3 @@
-/*
 provider "aws" {
   region = var.region
 }
@@ -34,30 +33,3 @@ resource "aws_instance" "ubuntu" {
     }
   }
 }
-*/
-
-terraform {
-  required_providers {
-    docker = {
-      source = "kreuzwerker/docker"
-      version = "~> 2.13.0"
-    }
-  }
-}
-
-provider "docker" {}
-
-resource "docker_image" "nginx" {
-  name         = "nginx:latest"
-  keep_locally = false
-}
-
-resource "docker_container" "nginx" {
-  image = docker_image.nginx.latest
-  name  = "tutorial"
-  ports {
-    internal = 80
-    external = 8000
-  }
-}
-
